246ab8a02919216d02e3f9e3f4012a62c459e0d1
pasumarti-sh <pasumarti.sh@northeastern.edu>
2025-04-18 13:07:44 -0400

    Training from a certain checkpoint

--- Misc scripts/checkpoint_train.py
+++ Misc scripts/checkpoint_train.py
@@ -3,13 +3,11 @@ from env_setup import make_env
 
 # Create the environment
 env = make_env(render=False)()
-# or if you had a vectorized env previously, replicate that setup accordingly
 
 # Load the checkpointed model
 model = SAC.load("models/robot_arm_10000_steps", env=env, device="cuda")
 
 # Continue training for more timesteps:
-# Note: set reset_num_timesteps=False so that the internal counter doesn't reset
 model.learn(total_timesteps=300000, reset_num_timesteps=False)
 
 # Save the updated model
